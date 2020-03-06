b8874ddb4f699ae0a4e08d4b64c00a9a573141a7
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-06 17:34:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,10 +2,10 @@ require_relative "./user.rb"
 
 class Student < User
   
-  knowledge = []
+  @@knowledge = []
   
-  def initialize()
-    knowledge = []
+  def initialize
+    @@knowledge = []
   end
   
 end
